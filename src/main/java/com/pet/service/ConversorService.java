6320a3d818076f694e548d36d8017467cebbbff2
Peterson Yoshioka <pmsyoshioka@gmail.com>
2026-06-22 18:05:06 -0300

    feat: adiciona testes unitarios

--- src/main/java/com/pet/service/ConversorService.java
+++ src/main/java/com/pet/service/ConversorService.java
@@ -22,4 +22,23 @@ public class ConversorService {
 		
 		return decimal;
 	}
+	
+	public String dec2bin(Integer decimal) {
+		// Caso especial: se o número já for zero, retorna "0" direto
+	    if (decimal == 0) {
+	        return "0";
+	    }
+	    
+	    String binario = "";
+	    
+	    // O laço continua enquanto o número for maior que zero
+	    while (decimal > 0) {
+	        int resto = decimal % 2;     // Pega o resto da divisão (será sempre 0 ou 1)
+	        binario = resto + binario;   // Concatena o resto À ESQUERDA da string (inverte a ordem automaticamente)
+	        decimal = decimal / 2;       // Divide o número por 2 para a próxima iteração
+	    }
+	    
+	    return binario;	
+	}
+
 }
--- src/test/java/com/pet/Bin2DecApplicationTests.java
+++ src/test/java/com/pet/Bin2DecApplicationTests.java
@@ -1,13 +1,44 @@
 package com.pet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pet.service.ConversorService;
 
 @SpringBootTest
 class Bin2DecApplicationTests {
 
+	@Autowired
+	private ConversorService service;
+	
 	@Test
-	void contextLoads() {
+	void deveConverterPara9() {
+		assertEquals(9, service.bin2dec("1001"));
+	}
+	
+	@Test
+	void deveReceberInteiroERetornarString() {
+		assertEquals("0", service.dec2bin(0));
+	}
+	
+	@Test
+	void deveConverter9Para1001() {
+		assertEquals("1001", service.dec2bin(9));
+	}
+	
+	@Test
+	void deveConverter0Para0() {
+		assertEquals("0", service.dec2bin(0));
 	}
 
 }
+
+
+
+
+
+
+
